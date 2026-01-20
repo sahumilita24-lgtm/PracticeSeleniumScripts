b9a9324e00d90424c701891d5d187922a1309dd4
milita <milit@192.168.1.7>
2026-01-20 22:06:36 +0530

    js executor

---
+++ AutomationFrameworkSelenium/src/test/java/ex13_Javascript_ex/Lab40_JS_code.java
@@ -0,0 +1,28 @@
+package ex13_Javascript_ex;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Lab40_JS_code {
+	
+	
+	@Test
+	public void jsexecuto()
+	{
+	WebDriver driver = new ChromeDriver();
+	JavascriptExecutor js = (JavascriptExecutor)driver;
+	js.executeScript("window.location = 'https://www.google.com'");
+	
+	driver.get("https://selectorshub.com/xpath-practice-page/");
+	js.executeScript("window.scrollBy(0,700)");
+	
+	String title= js.executeScript("return document.title").toString();
+	String url = js.executeScript("return document.URL").toString();
+	System.out.println("Title: " + title + " url: " + url);
+	//driver.close();
+	
+	}
+
+}
